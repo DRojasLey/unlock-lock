f338f9fdca0c2a2099a0b5c2fc629ec06b5c17cd
dfrojasley <dfrojas.feld@gmail.com>
2024-09-28 07:12:33 -0500

    Create unique select id's for correct data capture
    Selects were previously not unique, which would
    hinder data validation.
    
    Now the add and delete button will create unique
    select elements to be captured

--- clues.js
+++ clues.js
@@ -21,6 +21,7 @@ addClueButton.addEventListener('click', () =>{
     if (decryptButton.disabled === true) decryptButton.disabled = false;
 
     if(clueSectionMainContainer.childElementCount < 5 ) {
+        cluesCount++;
         addNewClue();
     } else {
         addClueButton.disabled = true ;
@@ -29,6 +30,7 @@ addClueButton.addEventListener('click', () =>{
 
 deleteClueButton.addEventListener('click', () => {
     if (clueSectionMainContainer.childElementCount > 0){
+        cluesCount--;
         deleteClue();
     } else {
         deleteClueButton.disabled = true;
@@ -72,18 +74,19 @@ const addNewClue = () => {
      * @param {number} optionsCount how many options for each select
      * @param {variable} sectionToPopulate DOM element to add the new elements
      * @param {boolean} labelFlag determines if the select will have or not a label
+     * @param {string} selectName name for the select being created
      */
-    const createSelect = ( selectCount , optionsCount, sectionToPopulate, labelFlag ) => {
+    const createSelect = ( selectCount , optionsCount, sectionToPopulate, labelFlag, selectName ) => {
         for (let jndex = 0; jndex < selectCount; jndex++) {
             const newSelect = document.createElement('select');
             if (labelFlag === true){
                 const newLabel = document.createElement('label');
                 newLabel.innerText = `number`;
-                newLabel.setAttribute('for',`clueNumbers${jndex}`);
+                newLabel.setAttribute('for',`clue${selectName}${jndex}-${cluesCount}`);
                 sectionToPopulate.appendChild(newLabel)
             }
-            newSelect.setAttribute('name', `clueNumbers${jndex}`);
-            newSelect.setAttribute('id', `clueNumbers${jndex}`);
+            newSelect.setAttribute('name', `clue${selectName}${jndex}-${cluesCount}`);
+            newSelect.setAttribute('id', `clue${selectName}${jndex}-${cluesCount}`);
             addOptionsToSelect( optionsCount , newSelect );
             sectionToPopulate.appendChild(newSelect)
         }
@@ -105,15 +108,15 @@ const addNewClue = () => {
         newSubSubtitle.innerHTML = 'Select Clue Numbers';
         newSubSubtitle.setAttribute('class', 'subSubtitle');
         newClueBox.appendChild(newSubSubtitle);
-        createSelect( 3, 10, newClueBox, true );
+        createSelect( 3, 10, newClueBox, true, 'Numbers' );
         newSubSubtitle2.innerHTML = 'How many correct?';
         newSubSubtitle2.setAttribute('class', 'subSubtitle');
         newClueBox2.appendChild(newSubSubtitle2);
-        createSelect( 1, 4, newClueBox2 );
+        createSelect( 1, 4, newClueBox2, false, 'Correct' );
         newSubSubtitle3.innerHTML = 'Correct Positions?';
         newSubSubtitle3.setAttribute('class', 'subSubtitle');
         newClueBox3.appendChild(newSubSubtitle3);
-        createSelect( 1, 4, newClueBox3 );
+        createSelect( 1, 4, newClueBox3, false, 'Position' );
         newClueContainer.setAttribute('class', 'clueContainer')
         newClueContainer.appendChild(newClueBox);
         newClueContainer.appendChild(newClueBox2);
